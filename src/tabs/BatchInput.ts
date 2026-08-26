0cb61934708f332ebf75f5aba1798855d7670b41
Death Fuel <108496936+DeathFuel@users.noreply.github.com>
2026-08-26 17:37:36 +0200

    better editor + loading screen

--- src/tabs/BatchInput.ts
+++ src/tabs/BatchInput.ts
@@ -1,3 +1,33 @@
+import {
+	EditorState
+} from "@codemirror/state"
+import {
+	EditorView,
+	keymap,
+	highlightSpecialChars,
+	highlightActiveLine,
+	lineNumbers,
+	highlightActiveLineGutter
+} from "@codemirror/view"
+import {
+	indentOnInput,
+	bracketMatching,
+	indentUnit
+} from "@codemirror/language"
+import {
+	defaultKeymap,
+	history,
+	historyKeymap,
+    insertTab,
+} from "@codemirror/commands"
+import {
+	highlightSelectionMatches
+} from "@codemirror/search"
+import {
+	closeBrackets,
+	closeBracketsKeymap
+} from "@codemirror/autocomplete"
+
 import TabBase from "./TabBase.ts";
 import htmlSrc from "./BatchInput.html?raw"
 
@@ -6,7 +36,7 @@ import Terminal from "./Terminal.ts"
 
 export default class BatchInput extends TabBase {
 
-	private batchInput: HTMLTextAreaElement;
+	private view: EditorView;
 
 	constructor() {
 		let node = document.createElement("div");
@@ -17,22 +47,46 @@ export default class BatchInput extends TabBase {
 		this.title.label = "Batch Input";
 
 		let sendButton = node.querySelector("#sendButton") as HTMLButtonElement;
-		let batchInput = node.querySelector("#batchInput") as HTMLTextAreaElement;
 		let clearOnSend = node.querySelector("#clearOnSend") as HTMLInputElement;
 
-		this.batchInput = batchInput;
+		let view = new EditorView({
+			extensions: [
+				lineNumbers(),
+				highlightSpecialChars(),
+				history(),
+				indentOnInput(),
+				indentUnit.of("    "),
+				EditorState.tabSize.of(4),
+				bracketMatching(),
+				closeBrackets(),
+				highlightActiveLine(),
+				highlightActiveLineGutter(),
+				highlightSelectionMatches(),
+				keymap.of([
+					...closeBracketsKeymap,
+					...defaultKeymap,
+					...historyKeymap,
+					//indentWithTab
+					{ key: "Tab", run: insertTab }
+				]),
+			],
+		});
+		this.view = view;
+		node.insertBefore(view.dom, node.firstChild);
 
 		sendButton.addEventListener("click", function(_) {
 			if (clearOnSend.checked) {
 				Module.RecreateEnvironment();
 				TabBase.getInstance(Terminal).setupOutputLog();
 			}
-			Module.LoadAndExecute(Environment, batchInput.value);
+			Module.LoadAndExecute(Environment, view.state.doc.toString());
 		});
 	}
 
 	public setBatchInput(str: string) {
-		this.batchInput.value = str;
+		this.view.dispatch({
+			changes: { from: 0, to: this.view.state.doc.length, insert: str }
+		});
 	}
 }
 
